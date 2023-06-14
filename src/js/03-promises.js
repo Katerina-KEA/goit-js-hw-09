5609f7efd0416fd8478d117a0da3f931a23d9073
Katerina-KEA <diamondekku@gmail.com>
2023-06-15 00:58:38 +0300

    fixed_mistakes_promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,18 +16,20 @@ function createPromise(position, delay) {
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const delay = parseInt(event.target.elements.delay.value);
+  let delay = parseInt(event.target.elements.delay.value);
   const step = parseInt(event.target.elements.step.value);
   const amount = parseInt(event.target.elements.amount.value);
 
   for (let i = 0; i < amount; i += 1) {
-    createPromise(i + 1, delay + 1 * step)
+    const currentPosition = i + 1;
+    createPromise(currentPosition , delay )
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+    delay += step;
   }
 }
 form.addEventListener('submit', onFormSubmit);
